fcc54ad549dfc2a6d7f3f71c20caaa3544e4cfac
YemCan <yemcanycc@gmail.com>
2025-12-11 22:20:29 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'ycc-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/English/index.html',
+  '/English/style.css',
+  '/English/script.js',
+  '/Arabic/index.html',
+  '/Arabic/style.css',
+  '/Arabic/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
